54a1a7ed4297ad048fca3804965fd10471c0c472
mk39-png <113801254+mk39-png@users.noreply.github.com>
2025-11-30 13:01:18 -0600

    refactor: clarify global pytest fixtures pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 """
 Holding utility methods and "global" constants that pertain to all tests.
+So, this entails .obj reading an filepath resolving, along with NumPy test comparisons.
+A more concrete use case for this file is to initialize objects that are
+used throughout various tests (e.g. Spot Control .obj file)
 """
-
 
 import logging
 import os
+import pathlib
 
 import igl
 import numpy as np
@@ -19,41 +22,66 @@ from pyalgcon.core.affine_manifold import AffineManifold
 from pyalgcon.core.apply_transformation import (
     apply_camera_frame_transformation_to_vertices,
     apply_transformation_to_vertices)
-from pyalgcon.core.common import MatrixNx3f
+from pyalgcon.core.common import Matrix3x3f, MatrixNx3f
 from pyalgcon.quadratic_spline_surface.optimize_spline_surface import \
     OptimizationParameters
 from pyalgcon.quadratic_spline_surface.twelve_split_spline import (
     TwelveSplitSplineSurface, compute_twelve_split_spline_patch_boundary_edges)
-
-# from utils.projected_curve_networks_utils import SVGOutputMode
-
 
 logger: logging.Logger = logging.getLogger(__name__)
 
 
 FILE_BASE = "algebraic_contours"
 
+#
+# UTILITY METHODS
+#
 
-def initialize_spot_control_mesh() -> tuple[npty.ArrayLike, npty.ArrayLike, npty.ArrayLike, npty.ArrayLike]:
-    """ 
-    Used for testing spot_control mesh in generating 
-    the TwelveSplitSplineSurface.
-    Returns only the parts of the mesh that are needed
 
-    :return: tuple V, uv, F, FT
-    :rtype: tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+@pytest.fixture(scope="session", params=[
+    ("spot_control", "spot_control_mesh-cleaned_conf_simplified_with_uv.obj")
+])
+def obj_fileinfo(request) -> tuple[pathlib.Path, pathlib.Path]:
+    """ Flexible method to resolve filepath of test data.
+
+    :returns: tuple of folder path and filepath to a given obj file
     """
-    # Get input mesh
-    filename: str = "spot_control_mesh-cleaned_conf_simplified_with_uv.obj"
-    filepath: str = os.path.abspath(f"spot_control\\{filename}")
+    folder_name: str
+    file_name: str
+    folder_name, file_name = request.param
+    base_directory: pathlib.Path = pathlib.Path(__file__).parent / "data"
 
+    return (base_directory / folder_name), (base_directory / folder_name / file_name)
+
+
+@pytest.fixture(scope="session")
+def parsed_control_mesh(obj_fileinfo) -> tuple[pathlib.Path,
+                                               tuple[np.ndarray,
+                                                     np.ndarray,
+                                                     np.ndarray,
+                                                     np.ndarray]]:
+    """ 
+    Used for testing control mesh in generating 
+    the TwelveSplitSplineSurface.
+    Returns only the parts of the mesh that are needed.
+    Returns the root folder of the mesh and its associated parsed
+    data.
+
+    :return: folder path and tuple V, uv, F, FT
+    :rtype: pathlib.Path, tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
+    """
+    folder_path: pathlib.Path
+    file_path: pathlib.Path
+    folder_path, file_path = obj_fileinfo
+
+    # Get input mesh
     V_temp: npty.ArrayLike
     uv_temp: npty.ArrayLike
     N_temp: npty.ArrayLike
     F_temp: npty.ArrayLike  # int
     FT_temp: npty.ArrayLike  # int
     FN_temp: npty.ArrayLike  # int
-    V_temp, uv_temp, N_temp, F_temp, FT_temp, FN_temp = igl.readOBJ(filepath)
+    V_temp, uv_temp, N_temp, F_temp, FT_temp, FN_temp = igl.readOBJ(file_path)
 
     # Wrapping inside np.array for typing
     V: np.ndarray = np.array(V_temp)
@@ -61,173 +89,64 @@ def initialize_spot_control_mesh() -> tuple[npty.ArrayLike, npty.ArrayLike, npty
     F: np.ndarray = np.array(F_temp)
     FT: np.ndarray = np.array(FT_temp)
 
-    return V, uv, F, FT
+    return folder_path, (V, uv, F, FT)
 
 
-# **************
-# Helper Methods Compute Contours
-# **************
-def _initialize_contour_info_spot_mesh() -> tuple[TwelveSplitSplineSurface,
-                                                  IntersectionParameters,
-                                                  InvisibilityParameters,
-                                                  list[tuple[int, int]],
-                                                  np.ndarray]:
-    """
-    Helper function to initialize information for Contour Network testing.
-
-    :return: spline_surface
-    :return: intersect_params
-    :return: invisibility_params
-    :return: patch_boundary_edges
-    :return: frame
-    """
-    # svg_output_mode: SVGOutputMode = SVGOutputMode.UNIFORM_VISIBLE_CURVES
-    optimization_params = OptimizationParameters()
-    intersect_params = IntersectionParameters()
-    invisibility_params = InvisibilityParameters()
-    # weight: float = optimization_params.position_difference_factor
-    # trim: float = intersect_params.trim_amount
-    # pad: float = invisibility_params.pad_amount
-    # invisibility_method: InvisibilityMethod = invisibility_params.invisibility_method
-    # show_nodes: bool = False
-    logger.setLevel(logging.INFO)
-
-    # Set up the camera
-    frame_3x3f: np.ndarray = np.array([[1, 0, 0],
-                                       [0, 1, 0],
-                                       [0, 0, 1]])
-    logger.info("Projecting onto frame:\n%s", frame_3x3f)
-    V:  np.ndarray
-    uv: np.ndarray
-    F:  np.ndarray
-    FT: np.ndarray
-    V, uv, F, FT = initialize_spot_control_mesh()
-    V_transformed: MatrixNx3f = apply_camera_frame_transformation_to_vertices(V, frame_3x3f)
-
-    # Generate quadratic spline
-    logger.info("Comnputing spline surface")
-    affine_manifold: AffineManifold = AffineManifold(F, uv, FT)
-    spline_surface: TwelveSplitSplineSurface = TwelveSplitSplineSurface(V_transformed,
-                                                                        affine_manifold,
-                                                                        optimization_params)
-
-    # Get the boundary edges
-    patch_boundary_edges: list[tuple[int, int]]
-    patch_boundary_edges: list[tuple[int, int]] = (
-        compute_twelve_split_spline_patch_boundary_edges(F, spline_surface.face_to_patch_indices))
-
-    return spline_surface, intersect_params, invisibility_params, patch_boundary_edges, frame_3x3f
-
-
-#
-# Affine Manifold
-#
-@pytest.fixture
-def initialize_affine_manifold_from_spot_control() -> AffineManifold:
-    """
-    Helper function to initialize AffineManifold from spot_control mesh.
-    """
-    # Get input mesh
-    V, uv, F, FT = initialize_spot_control_mesh()
-    affine_manifold: AffineManifold = AffineManifold(F, uv, FT)
-    affine_manifold_filepath: str = "spot_control\\affine_manifold\\"
-
-    return affine_manifold
-
-
-#
-# TESTING PARAMETERS
-#
-@pytest.fixture
-def obj_filepaths() -> list[str]:
-    print(os.path.abspath(f'{FILE_BASE}'))
-    return [
-        # (os.path.abspath(
-        #     f'{FILE_BASE}\\tests\\spot_control\\spot_control_mesh-cleaned_conf_simplified_with_uv.obj')),
-        (os.path.abspath("default_cube\\temp_out.obj"))
-    ]
-# @pytest.mark.parametrize(
-#     "filepath",
-#     [
-#         # (os.path.abspath(
-#         #     f'{FILE_BASE}\\tests\\spot_control\\spot_control_mesh-cleaned_conf_simplified_with_uv.obj')),
-#         (os.path.abspath(
-#             f'{FILE_BASE}\\tests\\default_cube\\temp_out.obj')),
-#     ]
-# )
-
-# XXX: change to proper mark
-
-
-@pytest.fixture
-def projection_matrices() -> list[np.ndarray]:
-    return [np.array([
-        [2.777777671813965, 0.0, 0.0, 0.0],
-        [0.0, 4.938271522521973, 0.0, 0.0],
-        [0.0, 0.0, -1.0020020008087158, -0.20020020008087158],
-        [0.0, 0.0, -1.0, 0.0]], dtype=np.float64)]
-# @pytest.mark.parametrize(
-#     "projection_matrices",
-#     [
-#         # TODO: this may not correspond to the Blender's actual projection matrix.
-#         # That I need to test.
-#         np.array([
-#             [2.777777671813965, 0.0, 0.0, 0.0],
-#             [0.0, 4.938271522521973, 0.0, 0.0],
-#             [0.0, 0.0, -1.0020020008087158, -0.20020020008087158],
-#             [0.0, 0.0, -1.0, 0.0]], dtype=np.float64)
-#     ]
-# )
-#
-# TESTING FIXTURES
-#
-
-
-@pytest.fixture
-def root_folder() -> str:
-    # FIXME: this should be limited in scope to the fixtures below rather than it being
-    # accessible by all other pytest cases
-    return "spot_control"
-
-
-@pytest.fixture(name="load_mesh_testing")
-def load_mesh_testing(obj_filepaths) -> tuple[npty.ArrayLike,
-                                              npty.ArrayLike,
-                                              npty.ArrayLike,
-                                              npty.ArrayLike,
-                                              npty.ArrayLike,
-                                              npty.ArrayLike]:
-    """
-    Returns deserialized .obj file.
-    """
-    return igl.readOBJ(obj_filepaths[0])
-
-
-@pytest.fixture
-def initialize_affine_manifold(load_mesh_testing) -> AffineManifold:
+@pytest.fixture(scope="session")
+def initialize_affine_manifold(parsed_control_mesh) -> AffineManifold:
     """
     Fixture to calculate the AffineManifold from the load_mesh_testing fixture.
     """
-    V_raw, uv, N, F, FT, FN = load_mesh_testing
+    folder_path, (V, uv, F, FT) = parsed_control_mesh
     affine_manifold: AffineManifold = AffineManifold(F, uv, FT)
     return affine_manifold
 
 
-@pytest.fixture(name="projection_on_vertices")
-def projection_on_vertices(load_mesh_testing, projection_matrices) -> np.ndarray:
-    """
-    Returns vertices under projection matrix.
-    """
-    V_raw, uv, N, F, FT, FN = load_mesh_testing
-    projection_matrix: np.ndarray = projection_matrices[0]
+# @pytest.fixture
+# def projection_matrices() -> list[np.ndarray]:
+#     """Preset matrices from Blender to test with
 
-    V_transformed: MatrixNx3f = apply_transformation_to_vertices(V_raw, projection_matrix)
+#     :return: perspective matrix
+#     :rtype: list[np.ndarray]
+#     """
+#     return [np.array([
+#         [2.777777671813965, 0.0, 0.0, 0.0],
+#         [0.0, 4.938271522521973, 0.0, 0.0],
+#         [0.0, 0.0, -1.0020020008087158, -0.20020020008087158],
+#         [0.0, 0.0, -1.0, 0.0]], dtype=np.float64)]
+
+# TODO: modify the below to support various projection matrices.
+# @pytest.fixture(scope="session")
+# def projection_on_vertices(parsed_control_mesh, projection_matrices) -> np.ndarray:
+#     """
+#     Returns vertices under projection matrix.
+#     """
+#     folder_path, (V_raw, uv, F, FT) = parsed_control_mesh
+
+#     # TODO: have ability to test with various projection matrices
+#     projection_matrix: np.ndarray = projection_matrices[0]
+#     V_transformed: MatrixNx3f = apply_transformation_to_vertices(V_raw, projection_matrix)
+#     return V_transformed
+
+
+@pytest.fixture(scope="session")
+def projection_frame_on_vertices(parsed_control_mesh) -> np.ndarray:
+    """
+    Returns vertices projected onto camera default camera.
+    """
+    folder_path, (V_raw, uv, F, FT) = parsed_control_mesh
+
+    # TODO: have ability to test with various projection matrices
+    frame: Matrix3x3f = np.array([[1, 0, 0],
+                                  [0, 1, 0],
+                                  [0, 0, 1]])
+    V_transformed: MatrixNx3f = apply_camera_frame_transformation_to_vertices(V_raw, frame)
     return V_transformed
 
 
-@pytest.fixture
-def initialize_twelve_split_spline_transformed(initialize_affine_manifold,
-                                               projection_on_vertices) -> TwelveSplitSplineSurface:
+@pytest.fixture(scope="session")
+def twelve_split_spline_transformed(initialize_affine_manifold,
+                                    projection_frame_on_vertices) -> TwelveSplitSplineSurface:
     """
     This is used to test the member variables of TwevleSplitSplineSurface().
     Helper function that initialized TwelveSplitSplineSurface from the spot_control mesh.
@@ -238,7 +157,7 @@ def initialize_twelve_split_spline_transformed(initialize_affine_manifold,
     NOTE: initializes 12-split spline for use in contour network.
     """
     affine_manifold: AffineManifold = initialize_affine_manifold
-    V_transformed: np.ndarray = projection_on_vertices
+    V_transformed: np.ndarray = projection_frame_on_vertices
     optimization_params: OptimizationParameters = OptimizationParameters()
 
     # Generate quadratic spline
@@ -249,24 +168,26 @@ def initialize_twelve_split_spline_transformed(initialize_affine_manifold,
     return spline_surface_transformed
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def initialize_contour_network(
-        load_mesh_testing,
-        initialize_twelve_split_spline_transformed) -> ContourNetwork:
+        parsed_control_mesh,
+        twelve_split_spline_transformed) -> tuple[pathlib.Path, ContourNetwork]:
     """
     Used for testing of contour network generat
     """
     # Retrieve parameters
-    V_raw, uv, N, F, FT, FN = load_mesh_testing
-    spline_surface: TwelveSplitSplineSurface = initialize_twelve_split_spline_transformed
+    folder_path, (V_raw, uv, F, FT) = parsed_control_mesh
+    spline_surface: TwelveSplitSplineSurface = twelve_split_spline_transformed
     intersect_params = IntersectionParameters()
     invisibility_params = InvisibilityParameters()
 
     # Get the boundary edges
     patch_boundary_edges: list[tuple[int, int]] = (
-        compute_twelve_split_spline_patch_boundary_edges(F, spline_surface.face_to_patch_indices))
+        compute_twelve_split_spline_patch_boundary_edges(F, spline_surface.face_to_patch_indices)
+    )
 
     # Build the contours
+    logger.info("Computing contours")
     contour_network = ContourNetwork(
         spline_surface,
         intersect_params,
@@ -274,4 +195,5 @@ def initialize_contour_network(
         patch_boundary_edges
     )
 
-    return contour_network
+    output_filepath: pathlib.Path = folder_path / "contour_network" / "output.svg"
+    return output_filepath, contour_network
